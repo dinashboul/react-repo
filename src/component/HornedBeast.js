7ae53b0271b39275160c2bf3816a7c9952ae4f81
dinashboul <dinaalshboul@gmail.com>
2022-08-22 22:48:02 +0300

    code2

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,19 +1,47 @@
 import React from "react";
+// import myJson from './package.json' assert {type: 'json'};
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
+// console.log(myJson.person.name);
 class HornedBeast extends React.Component {
+
+    constructor(props) {
+        super(props); 
+        this.state = {
+            numOfPets : 0
+        }
+    }
+
+    incrementNumOfPets = () => {
+        this.setState({
+            numOfPets : this.state.numOfPets + 1
+        })
+    }
+    
     render() {
+
         return(
-            <div>
-            <h2>{this.props.title}</h2>
-            <p>{this.props.disc}</p>
-            <img src ={this.props.img} alt='horend 1'></img>
-            <style jsx>
-                {`img{
-                        width:700px;
-                        height :400px;
-                          }`}
-            </style>
-            </div>
+           
+            <Card style={{ width: '18rem',display:'inline-block' }} className="beast-card" >
+            <Card.Body class="card-deck" >
+            <Card.Title >{this.props.title}</Card.Title>
+            <Card.Img style={{ width: '50%' ,height:'50%'}} variant="top" src={this.props.img} onClick={this.incrementNumOfPets} />
+            <Card.Text>
+                    Numbers
+                </Card.Text>
+                <Card.Text>
+                    😸 Num of pets: {this.state.numOfPets}
+                </Card.Text>
+                <Card.Text>
+                     {this.props.description}
+                </Card.Text>
+               
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+        
             )
     }
 }
--- src/component/Main.js
+++ src/component/Main.js
@@ -2,16 +2,29 @@ import React from "react";
 import HornedBeast from './HornedBeast';
 // import h1 from './component/assest/h1.jpg';
 // import hh2 from './component/assest/hh2.jpg';
+import packJson from "./pack.json";
 
-class Main extends React.Component {
-    render() {
-        return(
-            <div>
-            <HornedBeast title ='h1' img ='https://barrelwisdom.com/media/games/sophie2/monsters/horned-beast.webp' disc='horend blue'></HornedBeast>
-            <HornedBeast title ='h2' img ='https://d1rw89lz12ur5s.cloudfront.net/photo/bigncollectibles/file/684ea6e399c14082ba9977c011361174/IMG_20210623_205748.jpg' disc='horend red'></HornedBeast>
-            </div>
+let Data = packJson.map(val => 
+
+
+    
+
+
+        <HornedBeast 
+            key = {val._id}
+            title = {val.title}
+            img = {val.image_url}
+            description = {val.description}
+        /> 
+    );
+    
+    class Main extends React.Component {
+        render(){
+            return(
+                Data
+                
             )
+        }
     }
-}
 
 export default Main;
